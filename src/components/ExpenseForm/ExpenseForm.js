6a5fff5baae9debcaed22375e12fa478a58c0cef
Satyajit Gokhale <twisterintwist@gmail.com>
2024-06-05 12:20:05 -0400

    after transformation

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,23 +1,21 @@
+import { memo } from "react";
 import React, { useEffect, useRef } from "react";
 import styles from "./ExpenseForm.module.css";
 import { toast } from "react-toastify";
-
-const ExpenseForm = ({
+const ExpenseForm = memo(({
   addExpense,
   expenseToUpdate,
   updateExpense,
-  resetExpenseToUpdate,
+  resetExpenseToUpdate
 }) => {
   const expenseTextInput = useRef();
   const expenseAmountInput = useRef();
-
   useEffect(() => {
     if (!expenseToUpdate) return;
     expenseTextInput.current.value = expenseToUpdate.text;
     expenseAmountInput.current.value = expenseToUpdate.amount;
   }, [expenseToUpdate]);
-
-  const onSubmitHandler = (e) => {
+  const onSubmitHandler = e => {
     e.preventDefault();
     const expenseText = expenseTextInput.current.value;
     const expenseAmount = expenseAmountInput.current.value;
@@ -29,18 +27,17 @@ const ExpenseForm = ({
       const expense = {
         text: expenseText,
         amount: expenseAmount,
-        id: new Date().getTime(),
+        id: new Date().getTime()
       };
       addExpense(expense);
       clearInput();
       toast.success("Expense added successfully!");
       return;
     }
-
     const expense = {
       text: expenseText,
       amount: expenseAmount,
-      id: expenseToUpdate.id,
+      id: expenseToUpdate.id
     };
     console.log(expense);
     const result = updateExpense(expense);
@@ -49,41 +46,22 @@ const ExpenseForm = ({
     resetExpenseToUpdate();
     toast.success("Expense updated successfully!");
   };
-
   const clearInput = () => {
     expenseAmountInput.current.value = "";
     expenseTextInput.current.value = "";
   };
-
-  return (
-    <form className={styles.form} onSubmit={onSubmitHandler}>
+  return <form className={styles.form} onSubmit={onSubmitHandler}>
       <h3>{expenseToUpdate ? "Edit " : "Add new "}transaction</h3>
       <label htmlFor="expenseText">Text</label>
-      <input
-        id="expenseText"
-        className={styles.input}
-        type="text"
-        placeholder="Enter text..."
-        ref={expenseTextInput}
-        required
-      />
+      <input id="expenseText" className={styles.input} type="text" placeholder="Enter text..." ref={expenseTextInput} required />
       <div>
         <label htmlFor="expenseAmount">Amount</label>
         <div>(negative - expense,positive-income)</div>
       </div>
-      <input
-        className={styles.input}
-        id="expenseAmount"
-        type="number"
-        placeholder="Enter amount..."
-        ref={expenseAmountInput}
-        required
-      />
+      <input className={styles.input} id="expenseAmount" type="number" placeholder="Enter amount..." ref={expenseAmountInput} required />
       <button className={styles.submitBtn}>
         {expenseToUpdate ? "Edit " : "Add "} Transaction
       </button>
-    </form>
-  );
-};
-
+    </form>;
+});
 export default ExpenseForm;
