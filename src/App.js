906105f5283c8fbb6deabb70b193fa9637f2106b
Satyajit Gokhale <twisterintwist@gmail.com>
2024-06-07 13:24:27 -0400

    after transformation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useCallback } from "react";
+import { memo } from "react";
 import { useState } from "react";
 import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
 import ExpenseInfo from "./components/ExpenseInfo/ExpenseInfo";
@@ -5,65 +7,43 @@ import ExpenseList from "./components/ExpenseList/ExpenseList";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-
-function App() {
+const App = memo(function App() {
   const [expenses, setExpenses] = useState([]);
   const [expenseToUpdate, setExpenseToUpdate] = useState(null);
-
-  const addExpense = (expense) => {
-    setExpenses((prevState) => [expense, ...prevState]);
-  };
-
-  const deleteExpense = (id) => {
-    setExpenses((prevExpenses) => {
-      return prevExpenses.filter((expense) => expense.id !== id);
+  const addExpense = useCallback(expense => {
+    setExpenses(prevState => [expense, ...prevState]);
+  }, [setExpenses]);
+  const deleteExpense = useCallback(id => {
+    setExpenses(prevExpenses => {
+      return prevExpenses.filter(expense => expense.id !== id);
     });
-  };
-
-  const resetExpenseToUpdate = () => {
+  }, [setExpenses]);
+  const resetExpenseToUpdate = useCallback(() => {
     setExpenseToUpdate(null);
-  };
-
-  const updateExpense = (expense) => {
-    const expensePos = expenses
-      .map(function (exp) {
-        return exp.id;
-      })
-      .indexOf(expense.id);
-
+  }, [setExpenseToUpdate]);
+  const updateExpense = useCallback(expense => {
+    const expensePos = expenses.map(function (exp) {
+      return exp.id;
+    }).indexOf(expense.id);
     if (expensePos === -1) {
       toast.error("Expense not found!");
       return false;
     }
-
     const expensesDuplicate = expenses;
     expensesDuplicate[expensePos] = expense;
     setExpenses(expensesDuplicate);
     return true;
-  };
-
-  return (
-    <>
+  }, [expenses, setExpenses]);
+  return <>
       <ToastContainer />
       <h2 className="mainHeading">Expense Tracker</h2>
       <div className="App">
-        <ExpenseForm
-          addExpense={addExpense}
-          expenseToUpdate={expenseToUpdate}
-          updateExpense={updateExpense}
-          resetExpenseToUpdate={resetExpenseToUpdate}
-        />
+        <ExpenseForm addExpense={addExpense} expenseToUpdate={expenseToUpdate} updateExpense={updateExpense} resetExpenseToUpdate={resetExpenseToUpdate} />
         <div className="expenseContainer">
           <ExpenseInfo expenses={expenses} />
-          <ExpenseList
-            expenses={expenses}
-            deleteExpense={deleteExpense}
-            changeExpenseToUpdate={setExpenseToUpdate}
-          />
+          <ExpenseList expenses={expenses} deleteExpense={deleteExpense} changeExpenseToUpdate={setExpenseToUpdate} />
         </div>
       </div>
-    </>
-  );
-}
-
+    </>;
+});
 export default App;
